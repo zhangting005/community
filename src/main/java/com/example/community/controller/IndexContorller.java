f91f3881374f5d8939fff59f86b77736ae268a00
zhangting005 <245892730@qq.com>
2019-10-10 14:41:02 +0800

    'backtoeclipse'

--- src/main/java/com/example/community/controller/IndexContorller.java
+++ src/main/java/com/example/community/controller/IndexContorller.java
@@ -1,12 +1,10 @@
 package com.example.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloContorller {
+public class IndexContorller {
 	
 	@GetMapping("/")
 	private String hello() {
